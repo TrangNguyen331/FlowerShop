7c092bcc73b022c01dd4caa0d91d5f7aef377f3f
tinnguyen <tinnguyen331@gmail.com>
2023-11-27 21:28:56 +0700

    intergrate blogdetail

--- FE/src/pages/blog/BlogDetailsStandard.js
+++ FE/src/pages/blog/BlogDetailsStandard.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import MetaTags from "react-meta-tags";
 import { BreadcrumbsItem } from "react-breadcrumbs-dynamic";
 import LayoutOne from "../../layouts/LayoutOne";
@@ -7,10 +7,34 @@ import Breadcrumb from "../../wrappers/breadcrumb/Breadcrumb";
 import BlogSidebar from "../../wrappers/blog/BlogSidebar";
 // import BlogComment from "../../wrappers/blog/BlogComment";
 import BlogPost from "../../wrappers/blog/BlogPost";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import NotFound from "../other/NotFound";
+import axiosInstance from "../../axiosInstance";
 
 const BlogDetailsStandard = ({ location }) => {
   const { pathname } = location;
-  console.log("BlogDetail page");
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(
+          "/api/v1/blogs/" + id
+        );
+        setData(response.data);
+      } catch (error) {
+        return (
+          <Fragment>
+            <NotFound path={"/blog-details-standard/" + id}></NotFound>
+          </Fragment>
+        )
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or error
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Fragment>
       <MetaTags>
@@ -24,29 +48,33 @@ const BlogDetailsStandard = ({ location }) => {
       <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
         Blog Post
       </BreadcrumbsItem>
-      <LayoutOne>
-        {/* breadcrumb */}
-        <Breadcrumb />
-        <div className="blog-area pt-100 pb-100">
-          <div className="container">
-            <div className="row flex-row-reverse">
-              <div className="col-lg-9">
-                <div className="blog-details-wrapper ml-20">
-                  {/* blog post */}
-                  <BlogPost />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <LayoutOne>
+          {/* breadcrumb */}
+          <Breadcrumb />
+          <div className="blog-area pt-100 pb-100">
+            <div className="container">
+              <div className="row flex-row-reverse">
+                <div className="col-lg-9">
+                  <div className="blog-details-wrapper ml-20">
+                    {/* blog post */}
+                    <BlogPost blogData={data} />
 
-                  {/* blog post comment */}
-                  {/* <BlogComment /> */}
+                    {/* blog post comment */}
+                    {/* <BlogComment /> */}
+                  </div>
                 </div>
-              </div>
-              <div className="col-lg-3">
-                {/* blog sidebar */}
-                <BlogSidebar />
+                <div className="col-lg-3">
+                  {/* blog sidebar */}
+                  <BlogSidebar />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </LayoutOne>
+        </LayoutOne>
+      )}
     </Fragment>
   );
 };
