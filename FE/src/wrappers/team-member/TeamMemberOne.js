9c19302c456fa0979b8edb9f8d933daa5148e170
Hanh <nguyenkimhanh1532@gmail.com>
2023-12-03 12:58:07 +0700

    update subtitle

--- FE/src/wrappers/team-member/TeamMemberOne.js
+++ FE/src/wrappers/team-member/TeamMemberOne.js
@@ -15,7 +15,7 @@ const TeamMemberOne = ({ spaceTopClass, spaceBottomClass }) => {
         {/* section title */}
         <SectionTitleTwo
           titleText="Team Members"
-          subTitleText="Lorem ipsum dolor sit amet conse ctetu."
+          subTitleText="We are passionate about making beautiful more beautiful."
           positionClass="text-center"
           spaceClass="mb-60"
         />
