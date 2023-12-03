c4c2c6201973e3567a1909b9499c3cea2ccb6963
Hanh <nguyenkimhanh1532@gmail.com>
2023-12-03 14:20:23 +0700

    delete compare

--- FE/src/components/product/ProductGridListSingle.js
+++ FE/src/components/product/ProductGridListSingle.js
@@ -11,12 +11,10 @@ const ProductGridListSingle = ({
   currency,
   addToCart,
   addToWishlist,
-  addToCompare,
   cartItem,
   wishlistItem,
-  compareItem,
   sliderClassName,
-  spaceBottomClass
+  spaceBottomClass,
 }) => {
   const [modalShow, setModalShow] = useState(false);
   const { addToast } = useToasts();
@@ -29,41 +27,22 @@ const ProductGridListSingle = ({
   return (
     <Fragment>
       <div
-        className={`col-xl-4 col-sm-6 ${sliderClassName ? sliderClassName : ""
-          }`}
+        className={`col-xl-4 col-sm-6 ${
+          sliderClassName ? sliderClassName : ""
+        }`}
       >
         <div
           className={`product-wrap ${spaceBottomClass ? spaceBottomClass : ""}`}
         >
           <div className="product-img">
             <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
-              <img
-                className="default-img"
-                src={product.images[0]}
-                alt=""
-              />
+              <img className="default-img" src={product.images[0]} alt="" />
               {product.images.length > 1 ? (
-                <img
-                  className="hover-img"
-                  src={product.images[1]}
-                  alt=""
-                />
+                <img className="hover-img" src={product.images[1]} alt="" />
               ) : (
                 ""
               )}
             </Link>
-            {product.discount || product.new ? (
-              <div className="product-img-badges">
-                {product.discount ? (
-                  <span className="pink">-{product.discount}%</span>
-                ) : (
-                  ""
-                )}
-                {product.new ? <span className="purple">New</span> : ""}
-              </div>
-            ) : (
-              ""
-            )}
 
             <div className="product-action">
               <div className="pro-same-action pro-wishlist">
@@ -129,16 +108,7 @@ const ProductGridListSingle = ({
               </Link>
             </h3>
             <div className="product-price">
-              {discountedPrice !== null ? (
-                <Fragment>
-                  <span>{finalDiscountedPrice+ " "+currency.currencySymbol}</span>{" "}
-                  <span className="old">
-                    {finalProductPrice +" "+currency.currencySymbol }
-                  </span>
-                </Fragment>
-              ) : (
-                <span>{finalProductPrice +" " +currency.currencySymbol } </span>
-              )}
+              <span>{finalProductPrice + " " + currency.currencySymbol} </span>
             </div>
           </div>
         </div>
@@ -186,18 +156,9 @@ const ProductGridListSingle = ({
                   </Link>
                 </h3>
                 <div className="product-list-price">
-                  {discountedPrice !== null ? (
-                    <Fragment>
-                      <span>
-                        { finalDiscountedPrice +" "+currency.currencySymbol}
-                      </span>{" "}
-                      <span className="old">
-                        { finalProductPrice +" "+currency.currencySymbol}
-                      </span>
-                    </Fragment>
-                  ) : (
-                    <span>{ finalProductPrice +" "+currency.currencySymbol} </span>
-                  )}
+                  <span>
+                    {finalProductPrice + " " + currency.currencySymbol}{" "}
+                  </span>
                 </div>
                 {product.rating && product.rating > 0 ? (
                   <div className="rating-review">
@@ -271,20 +232,6 @@ const ProductGridListSingle = ({
                       <i className="pe-7s-like" />
                     </button>
                   </div>
-                  <div className="shop-list-compare ml-10">
-                    <button
-                      className={compareItem !== undefined ? "active" : ""}
-                      disabled={compareItem !== undefined}
-                      title={
-                        compareItem !== undefined
-                          ? "Added to compare"
-                          : "Add to compare"
-                      }
-                      onClick={() => addToCompare(product, addToast)}
-                    >
-                      <i className="pe-7s-shuffle" />
-                    </button>
-                  </div>
                 </div>
               </div>
             </div>
@@ -302,10 +249,8 @@ const ProductGridListSingle = ({
         finaldiscountedprice={finalDiscountedPrice}
         cartitem={cartItem}
         wishlistitem={wishlistItem}
-        compareitem={compareItem}
         addtocart={addToCart}
         addtowishlist={addToWishlist}
-        addtocompare={addToCompare}
         addtoast={addToast}
       />
     </Fragment>
@@ -314,15 +259,13 @@ const ProductGridListSingle = ({
 
 ProductGridListSingle.propTypes = {
   addToCart: PropTypes.func,
-  addToCompare: PropTypes.func,
   addToWishlist: PropTypes.func,
   cartItem: PropTypes.object,
-  compareItem: PropTypes.object,
   currency: PropTypes.object,
   product: PropTypes.object,
   sliderClassName: PropTypes.string,
   spaceBottomClass: PropTypes.string,
-  wishlistItem: PropTypes.object
+  wishlistItem: PropTypes.object,
 };
 
 export default ProductGridListSingle;
