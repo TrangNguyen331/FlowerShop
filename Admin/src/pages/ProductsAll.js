286473b0358906418be3e8e85493ccfa83565992
Hanh <nguyenkimhanh1532@gmail.com>
2023-12-09 21:27:16 +0700

    update

--- Admin/src/pages/ProductsAll.js
+++ Admin/src/pages/ProductsAll.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PageTitle from "../components/Typography/PageTitle";
 import { Link, NavLink } from "react-router-dom";
-import {
-  EditIcon,
-  HomeIcon,
-  TrashIcon,
-} from "../icons";
+import { EditIcon, HomeIcon, TrashIcon } from "../icons";
 import {
   Card,
   CardBody,
-  Label,
-  Select,
   Button,
   TableBody,
   TableContainer,
@@ -36,7 +30,7 @@ import "../index.css";
 import AddForm from "../components/AddForm";
 import { useHistory } from "react-router-dom";
 import axiosInstance from "../axiosInstance";
-import {fa, tr} from "faker/lib/locales";
+import { fa, tr } from "faker/lib/locales";
 const ProductsAll = () => {
   const [view, setView] = useState("list");
 
@@ -45,8 +39,8 @@ const ProductsAll = () => {
   const [data, setData] = useState([]);
   // pagination setup
   const [resultsPerPage, setResultsPerPage] = useState(2);
-  const [totalPage, setTotalPage]=useState(0);
-  const [totalResults, setTotalResult]=useState(0)
+  const [totalPage, setTotalPage] = useState(0);
+  const [totalResults, setTotalResult] = useState(0);
   const [dataLoaded, setDataLoaded] = useState(false);
   //const totalResults = response.length;
 
@@ -58,34 +52,31 @@ const ProductsAll = () => {
 
   // on page change, load new sliced data
   // here you would make another server request for new data
-  const fetchData = async (page)=> {
+  const fetchData = async (page) => {
     try {
-      console.log("page",page);
-      const response = await axiosInstance
-          .get("/api/v1/products/paging?page="+(page-1)+
-              "&size="+resultsPerPage);
-      console.log("Response data",response.data);
+      console.log("page", page);
+      const response = await axiosInstance.get(
+        "/api/v1/products/paging?page=" + (page - 1) + "&size=" + resultsPerPage
+      );
+      console.log("Response data", response.data);
       setData(response.data.content);
       setPage(page);
       setTotalPage(response.data.totalPages);
       setTotalResult(response.data.totalElements);
       setDataLoaded(true);
+    } catch (error) {
+      console.log("Fetch data error", error);
     }
-    catch (error){
-      console.log("Fetch data error",error);
-    }
-  }
+  };
   useEffect(() => {
-
     fetchData(1);
     //setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage));
   }, []);
 
   async function onPageChange(p) {
-    console.log("Trigger on page change")
+    console.log("Trigger on page change");
     await fetchData(p);
   }
-
 
   function formatNumberWithDecimal(number) {
     // Convert the number to a string
@@ -95,7 +86,7 @@ const ProductsAll = () => {
     const groups = numString.split(/(?=(?:\d{3})+(?!\d))/);
 
     // Join the groups with a decimal point
-    const formattedNumber = groups.join('.');
+    const formattedNumber = groups.join(".");
 
     return formattedNumber;
   }
@@ -151,7 +142,7 @@ const ProductsAll = () => {
         </CardBody>
       </Card>
 
-      {/* Delete product modal */}
+      {/* Product modal */}
       <div className="modal">
         <Modal isOpen={isModalOpen} onClose={closeModal}>
           <ModalHeader className="flex items-center text-2xl">
@@ -197,100 +188,107 @@ const ProductsAll = () => {
       </div>
 
       {/* Product Views */}
-        <>
-          <TableContainer className="mb-8">
-            <Table>
-              <TableHeader>
-                <tr>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Description</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Collections</TableCell>
-                  <TableCell>Tags</TableCell>
-                  <TableCell>Action</TableCell>
-                </tr>
-              </TableHeader>
-              <TableBody>
-                {data.map((product) => (
-                  <TableRow
-                    key={product.id}
-                    className="hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors duration-200"
-                  >
-                    <TableCell>
-                      <div className="flex items-center text-sm">
+      <>
+        <TableContainer className="mb-8">
+          <Table>
+            <TableHeader>
+              <tr>
+                <TableCell>Name</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Collections</TableCell>
+                <TableCell>Tags</TableCell>
+                <TableCell>Action</TableCell>
+              </tr>
+            </TableHeader>
+            <TableBody>
+              {data.map((product) => (
+                <TableRow
+                  key={product.id}
+                  className="hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors duration-200"
+                >
+                  <TableCell>
+                    <div className="flex items-center text-sm">
+                      <Link to={`/app/product/${product.id}`}>
+                        <Avatar
+                          className="hidden mr-4 md:block"
+                          src={
+                            product &&
+                            product.images &&
+                            product.images.length > 0
+                              ? product.images[0]
+                              : ""
+                          }
+                          alt="Product image"
+                        />
+                      </Link>
+                      <div>
                         <Link to={`/app/product/${product.id}`}>
-                          <Avatar
-                            className="hidden mr-4 md:block"
-                            src={ product && product.images && product.images.length>0 ? product.images[0]: ""}
-                            alt="Product image"
-                          />
+                          <p className="font-semibold">{product.name}</p>
                         </Link>
-                        <div>
-                          <Link to={`/app/product/${product.id}`}>
-                            <p className="font-semibold">{product.name}</p>
-                          </Link>
-                        </div>
                       </div>
-                    </TableCell>
-                    <TableCell>
-                      {product.description}
-                    </TableCell>
-                    <TableCell className="text-sm">
-                      {formatNumberWithDecimal(product.price)}đ
-                    </TableCell>
-                    <TableCell className="text-sm">
-                      {product && product.collections && product.collections.length>0 ?(
-                         product.collections.map((collection,index)=>(
-                             <Badge type="success" key={index}>
-                               {collection}
-                             </Badge>
-                         ))
-                      ):("")}
-                    </TableCell>
-                    <TableCell className="text-sm">
-                      {product && product.tags && product.tags.length>0 ?(
-                          product.tags.map((tag,index)=>(
-                              <Badge type="success" key={index}>
-                                {tag}
-                              </Badge>
-                          ))
-                      ):("")}
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex">
-                        <Button
-                          icon={EditIcon}
-                          className="mr-3"
-                          layout="outline"
-                          aria-label="Edit"
-                          onClick={() => openModal("edit", product.id)}
-                        />
+                    </div>
+                  </TableCell>
+                  <TableCell className="whitespace-normal break-words">
+                    {product.description}
+                  </TableCell>
+                  <TableCell className="text-sm">
+                    {formatNumberWithDecimal(product.price)}đ
+                  </TableCell>
+                  <TableCell className="text-sm space-x-2">
+                    {product &&
+                    product.collections &&
+                    product.collections.length > 0
+                      ? product.collections.map((collection, index) => (
+                          <Badge type="success" key={index}>
+                            {collection}
+                          </Badge>
+                        ))
+                      : ""}
+                  </TableCell>
+                  <TableCell className="text-sm space-x-2">
+                    {product && product.tags && product.tags.length > 0
+                      ? product.tags.map((tag, index) => (
+                          <Badge type="success" key={index}>
+                            {tag}
+                          </Badge>
+                        ))
+                      : ""}
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex">
+                      <Button
+                        icon={EditIcon}
+                        className="mr-3"
+                        layout="outline"
+                        aria-label="Edit"
+                        onClick={() => openModal("edit", product.id)}
+                      />
 
-                        <Button
-                          icon={TrashIcon}
-                          layout="outline"
-                          aria-label="Delete"
-                          onClick={() => openModal("delete", product.id)}
-                        />
-                      </div>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-            <TableFooter>
-              {dataLoaded && (
-                  <Pagination
-                      totalResults={totalResults}
-                      resultsPerPage={resultsPerPage}
-                      label="Table navigation"
-                      onChange={(page) => onPageChange(page)}
-                  />
-              )}
-            </TableFooter>
-          </TableContainer>
-        </>
-
+                      <Button
+                        icon={TrashIcon}
+                        layout="outline"
+                        aria-label="Delete"
+                        onClick={() => openModal("delete", product.id)}
+                      />
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <TableFooter>
+            {dataLoaded && (
+              <Pagination
+                totalResults={totalResults}
+                resultsPerPage={resultsPerPage}
+                label="Table navigation"
+                onChange={(page) => onPageChange(page)}
+              />
+            )}
+          </TableFooter>
+        </TableContainer>
+      </>
     </div>
   );
 };
